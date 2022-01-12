e87b98b92b3d2967e725c4b1772378f3f7468711
Sanghun2 <50513500+Sanghun2@users.noreply.github.com>
2022-01-12 19:44:32 +0900

    카메라 이동까지

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float speed;
+
     float hAxis;
     float vAxis;
+    bool wDown;
 
     Vector3 moveVec;
 
-    void Start()
+    Animator anim;
+
+    void Awake()
     {
-        
+        anim = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
+        wDown = Input.GetButton("Walk"); //방향이 아니므로 Button 사용
+
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        transform.position += moveVec * speed * Time.deltaTime * (wDown ? 0.3f : 1f);
+
+        anim.SetBool("IsRun", moveVec != Vector3.zero);
+        anim.SetBool("IsWalk", wDown);
+
+        transform.LookAt(transform.position + moveVec);
     }
 }
